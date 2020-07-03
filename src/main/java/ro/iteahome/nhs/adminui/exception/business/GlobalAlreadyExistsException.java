664079482a75acb1584460eed04de239d2adc1be
Mihai Szocs <szocsmihai@gmail.com>
2020-07-03 11:42:54 +0300

    Working on exceptions.

--- src/main/java/ro/iteahome/nhs/adminui/exception/business/GlobalAlreadyExistsException.java
+++ src/main/java/ro/iteahome/nhs/adminui/exception/business/GlobalAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package ro.iteahome.nhs.adminui.exception.business;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.CONFLICT)
 public class GlobalAlreadyExistsException extends RuntimeException {
 
     private final String entityName;
--- src/main/java/ro/iteahome/nhs/adminui/exception/business/GlobalNotFoundException.java
+++ src/main/java/ro/iteahome/nhs/adminui/exception/business/GlobalNotFoundException.java
@@ -1,5 +1,9 @@
 package ro.iteahome.nhs.adminui.exception.business;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
 public class GlobalNotFoundException extends RuntimeException {
 
     private final String restEntity;
