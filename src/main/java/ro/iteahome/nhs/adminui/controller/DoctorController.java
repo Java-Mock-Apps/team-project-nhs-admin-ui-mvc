c5652744d34a26ee4a8fb3033247ef5a8b592ddd
Marius Bistika <marius.bistika@gmail.com>
2020-07-07 10:20:31 +0300

    deleted print lines

--- src/main/java/ro/iteahome/nhs/adminui/controller/DoctorController.java
+++ src/main/java/ro/iteahome/nhs/adminui/controller/DoctorController.java
@@ -87,9 +87,7 @@ public class DoctorController {
 
     @PostMapping("/updated-doctor")
     public ModelAndView update(@Valid Doctor doctor) {
-        System.out.println(doctor.getInstitutionCUIs());
         Doctor databaseDoctor = doctorService.update(doctor);
-        System.out.println(databaseDoctor.getInstitutionCUIs());
         databaseDoctor.setInstitutionCUIs(databaseDoctor.getInstitutions().stream()
                 .map(Institution::getName).collect(Collectors.joining(",")));
         return new ModelAndView("doctor/home-doctor").addObject(databaseDoctor);
